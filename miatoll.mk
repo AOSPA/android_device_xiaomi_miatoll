9a8dc5f25eb62b6ddc346dabd4b8d93745bcccca
Demon000 <demonsingur@gmail.com>
2020-08-13 21:23:11 +0300

    miatoll: use device specific thermal configuration map
    Change-Id: Ic5794cef136c70dec3f5f7341c9d4718a879cec1

--- miatoll.mk
+++ miatoll.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/thermal/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
 

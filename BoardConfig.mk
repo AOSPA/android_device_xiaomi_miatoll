1153729844e4818c78a20800a8026130ebe9ba99
Alexander Koskovich <zvnexus@outlook.com>
2021-04-27 06:57:55 -0700

    miatoll: Remove virtual framebuffer.
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,6 @@ BOARD_KERNEL_CMDLINE += \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000

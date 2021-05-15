2c2f122014f76a98ff53085fc5669102b189edc4
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-05-15 17:56:19 +0530

    miatoll: Switch to few prebuilt audio hals
    Change-Id: I4d9e6f9accb815b5562ca595b5badcfb7b36d717
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 
+# Audio
+TARGET_PROVIDES_AUDIO_EXTNS := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := miatoll
 

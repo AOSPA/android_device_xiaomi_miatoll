a6305f4421a4b7535c2fc3b48a3bcaa90478fe66
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-05-21 13:29:52 +0530

    miatoll: Decalre IR packages/permissions.
    Change-Id: I900dba7400d9e40b553c097704c7da31b60cdf50
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- miatoll.mk
+++ miatoll.mk
@@ -79,6 +79,14 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.1-service
 
+# IR
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom

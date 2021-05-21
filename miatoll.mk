183e36db157be9e9ea70c43086399b8f0f3b9b7e
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-05-21 12:49:21 +0530

    miatoll: Initial proprietary file listing.
    Change-Id: I66af043dc24e523bd2f94144789e48a3c7b6d7c6
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- miatoll.mk
+++ miatoll.mk
@@ -66,6 +66,9 @@ PRODUCT_PACKAGES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Vendor
+$(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wlan/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \

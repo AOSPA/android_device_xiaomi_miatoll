78681d7aa996be4dc09841ecf1a3785a3026c7d4
Michael Bestas <mkbestas@lineageos.org>
2021-08-14 18:09:58 +0530

    miatoll: Set androidboot.init_fatal_reboot_target=recovery
    * Don't crash to bootloader, users can access pstore in recovery in order to debug the issue.
    
    Change-Id: I33e91fa31477785ba9c14a56d1c7ee9110fcc0c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,8 @@ BOARD_KERNEL_CMDLINE += \
     service_locator.enable=1 \
     swiotlb=1
 
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000

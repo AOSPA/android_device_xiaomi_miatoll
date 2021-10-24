297f6470fa7d4b16488d49d0880fe2f3bacaa5ea
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-10-24 12:43:12 +0530

    [DONOTMERGE] miatoll: Disable updatable APEX
    Change-Id: I8206d1570ef4a1ed347f2d7c3ea28fe915ba9caf

--- miatoll.mk
+++ miatoll.mk
@@ -20,9 +20,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
 
-# APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \

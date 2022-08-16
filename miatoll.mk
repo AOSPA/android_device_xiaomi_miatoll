e7dc69fe795c93be094550dade13369fd28359e4
Bowgo Tsai <bowgotsai@google.com>
2022-08-17 00:40:54 +0600

    miatoll: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: tree hugger
    Change-Id: If0835179ab5332e94170f3cbdd1b708c9388e435

--- miatoll.mk
+++ miatoll.mk
@@ -160,9 +160,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # GPS
 LOC_HIDL_VERSION = 4.0
 
-# GSI
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \

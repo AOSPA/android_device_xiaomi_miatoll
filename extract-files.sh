4eafe56a68e4405ab80a3d87f1ac4bd2f743ae43
BladeRunner-A2C <john.smith@unused.email>
2023-07-15 01:03:41 +0600

    miatoll: Roll back to stock keymaster stack
    it didn't like the BSP stack for reasons unknown
    
    patch the 4.0 service init to load 4.1 keymaster lib,
    because stock doesn't have 4.1 service init
    
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- extract-files.sh
+++ extract-files.sh
@@ -56,6 +56,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
+            sed -i "s/4\.0/4\.1/g" "${2}"
+            ;;
         vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
             "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
             sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"

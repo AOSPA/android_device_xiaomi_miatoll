ab1cb458703fcad3221e64701905706a094dca23
BladeRunner-A2C <john.smith@unused.email>
2024-01-17 15:27:29 +0600

    miatoll: init: Update gram fingerprints
    Change-Id: I5e9e910f9b183bac8fc05e15422bb7461068b7e0
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- init/init_miatoll.cpp
+++ init/init_miatoll.cpp
@@ -73,7 +73,7 @@ void load_device_properties() {
         if (region == "Global_PA" || region == "Global_TWO" || region == "Japan") {
             set_device_props(
                 "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V14.0.4.0.SJWMIXM:user/release-keys",
-                "qssi-user-12-SKQ1.211019.001-V14.0.4.0.SJWMIXM-release-keys", "Redmi", "curtana",
+                "qssi-user 12 SKQ1.211019.001 V14.0.4.0.SJWMIXM-release-keys", "Redmi", "curtana",
                 "Redmi Note 9S");
         } else if (region == "India") {
             set_device_props(
@@ -88,8 +88,8 @@ void load_device_properties() {
             "Redmi Note 9 Pro Max");
     } else if (hwname == "gram") {
         set_device_props(
-            "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.3.0.SJPINXM:user/release-keys",
-            "qssi-user 12 SKQ1.211019.001 V14.0.3.0.SJPINXM release-keys", "POCO", "gram",
+            "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.5.0.SJPINXM:user/release-keys",
+            "qssi-user 12 SKQ1.211019.001 V14.0.5.0.SJPINXM release-keys", "POCO", "gram",
             "POCO M2 Pro");
     } else if (hwname == "joyeuse") {
         set_device_props(

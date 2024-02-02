9db05ccbf58e879a44dfe2e22ada6835b61942ac
BladeRunner-A2C <john.smith@unused.email>
2024-02-03 01:57:41 +0600

    miatoll: Build OSS audio hal
    Change-Id: I6ed655cc44f1fd3784e40ce216fb5ed800f271a0
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

--- init/init_miatoll.cpp
+++ init/init_miatoll.cpp
@@ -142,7 +142,6 @@ void load_dalvik_properties() {
 
 void load_audio_properties() {
     property_override("persist.vendor.audio.voicecall.speaker.stereo", "false");
-    property_override("vendor.audio.offload.track.enable", "false");
 }
 
 void vendor_load_properties() {
